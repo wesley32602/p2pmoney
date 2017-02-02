bef2b5923fd38aa451bcf1e76a5596e89f91ad46
sheepelysium <sheepelysium@gmail.com>
2017-02-02 16:12:37 +0800

    sh

---
+++ P2P借貸_MVC 3.0/P2P借貸_MVC/Areas/Forum/ForumAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace P2P借貸_MVC.Areas.Forum
+{
+    public class ForumAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Forum";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Forum_default",
+                "Forum/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
